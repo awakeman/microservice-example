2c8aee53c66d57a940145a65a8e5532d8f72db7b
Andrew Wakeman <andrew@wakeman.dev>
2024-05-30 12:20:12 +0100

    Add test projects

---
+++ Tests/SettingsService.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace SettingsService.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Tests/SignalRService.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace SignalRService.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
