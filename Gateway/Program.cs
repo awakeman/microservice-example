521d9382d472cdac35825fe8043f635b23495eec
Andrew Wakeman <andrew@wakeman.dev>
2024-05-30 13:50:54 +0100

    Configure Ocelot to route to the services

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,6 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Ocelot
+builder.Configuration.AddJsonFile("ocelot.json");
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Use ocelot routing
+await app.UseOcelot();
 
 app.Run();
